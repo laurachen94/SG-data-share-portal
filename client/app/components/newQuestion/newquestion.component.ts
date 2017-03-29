a658fd793584024c1e028022b96516cf13a570c3
laurachen94 <lchena@outllok.com>
2017-03-29 13:38:30 -0400

    FORTH BLOOD
    ADD NEW QUESTION FUNCTION

--- client/app/components/newQuestion/newquestion.component.ts
+++ client/app/components/newQuestion/newquestion.component.ts
@@ -2,6 +2,7 @@
  * Created by jeanliu on 3/22/17.
  */
 import {Component} from '@angular/core';
+import { QuestionsComponent} from '../questions/questions.components'
 
 @Component({
   moduleId: module.id,
@@ -9,7 +10,6 @@ import {Component} from '@angular/core';
   templateUrl: './newquestion.component.html',
   styleUrls: ['../../stylesheet.css']
 })
-
 export class NewQuestionComponent {
   title = "Create New Question";
 }
